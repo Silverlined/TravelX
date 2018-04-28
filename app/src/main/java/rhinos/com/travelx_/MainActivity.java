86ce4a9feb71cf4713cfdf7e3cd9ab7fc9492c70
Silverlined <dimitriy.georgiev@gmail.com>
2018-04-28 10:35:58 +0300

    FullScreen main activity + added second activity

---
+++ app/src/main/java/rhinos/com/travelx_/MainActivity.java
@@ -0,0 +1,42 @@
+package rhinos.com.travelx_;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setFullScreen();
+
+        ImageView hubbleView = (ImageView) findViewById(R.id.hubble);
+        hubbleView.setOnClickListener(v -> goToSecondActivity());
+    }
+
+    private void setFullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    private void goToSecondActivity() {
+        Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(startIntent);
+    }
+}
