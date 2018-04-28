33c3ed7027c332c44ada4671d13dfb8a7503b604
Silverlined <dimitriy.georgiev@gmail.com>
2018-04-28 16:47:52 +0300

    LaunchScreen

--- app/src/main/java/rhinos/com/travelx_/MainActivity.java
+++ app/src/main/java/rhinos/com/travelx_/MainActivity.java
@@ -1,11 +1,9 @@
 package rhinos.com.travelx_;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setFullScreen();
 
-        ImageView hubbleView = (ImageView) findViewById(R.id.hubble);
-        hubbleView.setOnClickListener(v -> goToSecondActivity());
+        ImageView skyfall = (ImageView) findViewById(R.id.skyfall);
+        skyfall.setOnClickListener(v -> goToSecondActivity());
     }
 
     private void setFullScreen() {
