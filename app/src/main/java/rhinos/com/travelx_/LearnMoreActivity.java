66063c7042fdc001b192b9553e62bae861ba9651
Silverlined <dimitriy.georgiev@gmail.com>
2018-04-29 03:01:12 +0300

    changed picture + added ending activity

--- app/src/main/java/rhinos/com/travelx_/LearnMoreActivity.java
+++ app/src/main/java/rhinos/com/travelx_/LearnMoreActivity.java
@@ -1,7 +1,8 @@
 package rhinos.com.travelx_;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LearnMoreActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class LearnMoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_more);
+        setFullScreen();
+    }
+
+    private void setFullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
 }
